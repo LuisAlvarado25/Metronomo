1e58f33ee5b90cd57f57a4f63f26449e005332f6
Luis Alvarado <100969224+LuisAlvarado25@users.noreply.github.com>
2025-09-24 17:59:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // *** 1. CONFIGURACIÓN Y DECLARACIÓN DE VARIABLES ***
 // ----------------------------------------------------
 const clientId = '9643b3fc5b11421da0364a8eff9b8545';
-const redirectUri = 'https://luisalvarado25.github.io/Metronomo/'; 
+const redirectUri = 'https://luisalvarado25.github.io/Metronomo/index.html'; 
 const scope = 'streaming user-read-playback-state user-modify-playback-state playlist-read-private';
 
 const loginBtn = document.getElementById('loginBtn');
@@ -449,4 +449,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
   metroDisplay.textContent = "Metrónomo: Inactivo";
 
 });
+
 
